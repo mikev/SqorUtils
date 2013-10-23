2d706389d9e99609ee3389fd3fc24eeecdfd0101
Kirk Woll <kirk.woll@sqor.com>
2013-10-23 06:24:27 -0700

    Misc

---
+++ EntityFramework/DbSetExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace Sqor.Utils.EntityFramework
+{
+    public static class DbSetExtensions
+    {
+        public static void RemoveRange<T>(this DbSet<T> dbSet, IEnumerable<T> entities) where T : class
+        {
+            foreach (var entity in entities)
+            {
+                dbSet.Remove(entity);
+            }
+        }
+    }
+}
---
+++ Objects/ObjectExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace Sqor.Utils.Objects
+{
+    public static class ObjectExtensions
+    {
+        public static bool SafeEquals<T>(this T o1, T o2)
+        {
+            bool o1Null = o1 == null;//EqualityComparer<T>.Default.Equals(o1, default(T));
+            bool o2Null = o2 == null;//EqualityComparer<T>.Default.Equals(o2, default(T));
+            if (o1Null && o2Null)
+                return true;
+            if (o1Null)
+                return false;
+
+            return o1.Equals(o2);
+        }         
+    }
+}
