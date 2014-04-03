f85df3f8193ded3255093ee0d3cf439b4a9c4c0e
Kirk Woll <kirk.woll@gmail.com>
2014-04-03 09:07:15 -0700

    Add sha algorithm (for stats inc)

---
+++ Encryption/SHA256Hash.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Sqor.Utils.Encryption
+{
+    public static class SHA256Hash
+    {
+        public static string Create(string input)
+        {
+            var sha = SHA256.Create();
+            var inputBytes = Encoding.ASCII.GetBytes(input);
+            var hashBytes = sha.ComputeHash(inputBytes);
+            var result = string.Join("", hashBytes.Select(x => x.ToString("X2")));
+            return result;
+        }
+    }
+}
