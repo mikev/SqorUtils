581661a1965578dda2d7f1d960f04f7b72254048
Joseph Dwyer <joe.dwyer@sqor.com>
2014-11-14 14:51:19 -0800

    force png format for crosspost images to they look nice.

--- Images/ImageExtensions.cs
+++ Images/ImageExtensions.cs
@@ -378,7 +378,7 @@ namespace Sqor.Utils.Images
 
                 if (transform.HasFlag(ImageTransform.CrosspostPresentation))
                 {
-                    image.Quality = 95;
+                    image.Format = MagickFormat.Png;
                     ConvertToCrosspostingRepresentation(image, bytes);
                 }
 
