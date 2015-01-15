78d5cbdd50038607855bb21d7c175eaa561d5ce0
Kirk Woll <kirk.woll@sqor.com>
2015-01-14 16:20:40 -0800

    Add mini profiler

---
+++ MiniProfiler/MiniProfilerRequestHeader.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Sqor.Utils.MiniProfiler
+{
+    public class MiniProfilerRequestHeader
+    {
+        /// <summary>
+        /// The header name.
+        /// </summary>
+        public const string HeaderName = "MiniProfilerRequestHeader";
+
+        /// <summary>
+        /// The header namespace.
+        /// </summary>
+        public const string HeaderNamespace = "StackExchange.Profiling.Wcf";
+
+        /// <summary>
+        /// Gets or sets the parent profiler id.
+        /// </summary>
+        public string ParentProfilerId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the user.
+        /// The name of the user as provided 
+        /// </summary>
+        public string User { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether exclude trivial methods.
+        /// </summary>
+        public bool ExcludeTrivialMethods { get; set; }
+
+        /// <summary>
+        /// Gets or sets the trivial duration threshold milliseconds.
+        /// </summary>
+        public decimal? TrivialDurationThresholdMilliseconds { get; set; }
+
+        /// <summary>
+        /// parse the header text, and return the resulting profiler.
+        /// </summary>
+        /// <param name="text">The text.</param>
+        /// <returns>the mini request header</returns>
+        public static MiniProfilerRequestHeader FromHeaderText(string text)
+        {
+            var parts = text.Split('&');
+            var header = new MiniProfilerRequestHeader
+                             {
+                                 ParentProfilerId = parts[0],
+                                 User = parts[1],
+                                 ExcludeTrivialMethods = parts[2] == "y"
+                             };
+
+            if (parts.Length > 3)
+                header.TrivialDurationThresholdMilliseconds = decimal.Parse(parts[3]);
+
+            return header;
+        }
+
+        /// <summary>
+        /// return the header text.
+        /// </summary>
+        /// <returns>a string containing the header text.</returns>
+        public string ToHeaderText()
+        {
+            var text = 
+                Convert.ToString(ParentProfilerId) + "&" + 
+                User + "&" 
+                + (ExcludeTrivialMethods ? "y" : "n") 
+                + (TrivialDurationThresholdMilliseconds.HasValue ? "&" + Convert.ToString(TrivialDurationThresholdMilliseconds.Value) : string.Empty);
+
+            return text;
+        }    }
+}
---
+++ MiniProfiler/MiniProfilerResultsHeader.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Sqor.Utils.MiniProfiler
+{
+    public class MiniProfilerResultsHeader
+    {
+        public const string HeaderName = "MiniProfilerResults";
+
+        /// <summary>
+        /// The header namespace used to serialize the data
+        /// </summary>
+        public const string HeaderNamespace = "StackExchange.Profiling.Wcf";
+
+        /// <summary>
+        /// Gets or sets the profiler results.
+        /// </summary>
+        public StackExchange.Profiling.MiniProfiler ProfilerResults { get; set; }
+
+        /// <summary>
+        /// Convert the supplied compressed and encoded string into a profile header.
+        /// </summary>
+        /// <param name="text">The text.</param>
+        /// <returns>The <c>deserialized</c> instance.</returns>
+        public static MiniProfilerResultsHeader FromHeaderText(string text)
+        {
+            byte[] buffer = Convert.FromBase64String(text);
+            buffer = Decompress(buffer);
+            return Read(buffer);
+        }
+
+        /// <summary>
+        /// Convert the header to a text string suitable for placement in an HTTP header.
+        /// </summary>
+        /// <returns>The compressed and encoded string of serialized data</returns>
+        public string ToHeaderText()
+        {
+            byte[] buffer = Write(this);
+            buffer = Compress(buffer);
+            return Convert.ToBase64String(buffer);
+        }
+
+        /// <summary>
+        /// Read a mini profiler results header from the supplied buffer.
+        /// </summary>
+        /// <param name="buffer">The buffer.</param>
+        /// <returns>The results header.</returns>
+        private static MiniProfilerResultsHeader Read(byte[] buffer)
+        {
+            using (var stream = new MemoryStream(buffer))
+            using (var reader = new StreamReader(stream))
+            {
+                var text = reader.ReadToEnd();
+                return JsonConvert.DeserializeObject<MiniProfilerResultsHeader>(text);
+            }
+        }
+
+        /// <summary>
+        /// Write the header to a byte stream.
+        /// </summary>
+        /// <param name="header">The header.</param>
+        /// <returns>The serialized data.</returns>
+        private static byte[] Write(MiniProfilerResultsHeader header)
+        {
+            using (var stream = new MemoryStream())
+            {
+                using (var writer = new StreamWriter(stream, Encoding.Unicode))
+                {
+                    writer.Write(JsonConvert.SerializeObject(header));
+                    writer.Flush();
+                    byte[] buffer = stream.GetBuffer();
+                    Array.Resize(ref buffer, (int)stream.Length);
+                    return buffer;
+                }
+            }            
+        }
+
+        /// <summary>
+        /// Compress the supplied array of bytes into a smaller packet of data.
+        /// </summary>
+        /// <param name="buffer">The buffer.</param>
+        /// <returns>The compressed data.</returns>
+        private static byte[] Compress(byte[] buffer)
+        {
+            using (var outStream = new MemoryStream())
+            {
+                using (var tinyStream = new GZipStream(outStream, CompressionMode.Compress))
+                using (var stream = new MemoryStream(buffer))
+                {
+                    stream.CopyTo(tinyStream);
+                }
+                
+                return outStream.ToArray();
+            }            
+        }
+
+        /// <summary>
+        /// decompress the supplied buffer into a smaller packet of data.
+        /// </summary>
+        /// <param name="buffer">The buffer.</param>
+        /// <returns>The uncompressed data.</returns>
+        private static byte[] Decompress(byte[] buffer)
+        {
+            using (var stream = new MemoryStream(buffer))
+            using (var zipStream = new GZipStream(stream, CompressionMode.Decompress))
+            using (var outputStream = new MemoryStream())
+            {
+                zipStream.CopyTo(outputStream);
+                return outputStream.ToArray();
+            }
+        }         
+    }
+}
